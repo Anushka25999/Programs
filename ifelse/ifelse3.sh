2238dedff7a0fc3b03689a54954c3afd106d077c
Anushka25999 <48594364+Anushka25999@users.noreply.github.com>
2020-08-21 18:43:43 +0530

    Update and rename ifelse3.sh to ifelse/ifelse3.sh

--- ifelse/ifelse3.sh
+++ ifelse/ifelse3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+
+#program that takes a year as input and outputs the year is a leap year or not a leap year
 read -p "Enter a year: " year;
 if [ $year -ge 1000 -a $year -le 9999 ]; then
 	if [ $(( year % 4 )) -eq 0 ]; then

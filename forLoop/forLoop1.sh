170e7316455fe606fd6eb14397b77af6c6a849b9
Anushka25999 <48594364+Anushka25999@users.noreply.github.com>
2020-08-21 19:24:05 +0530

    Update and rename forLoop1.sh to forLoop/forLoop1.sh

--- forLoop/forLoop1.sh
+++ forLoop/forLoop1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+
+#Program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n
 read -p "Enter a number: " n;
 power=1;
 for (( i=0; i<=$n; i++ ))

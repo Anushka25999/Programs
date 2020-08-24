7637978b430b8a6fbf1c630f06304135103b249d
Anushka25999 <48594364+Anushka25999@users.noreply.github.com>
2020-08-24 15:17:35 +0530

    Update and rename whileLoop1.sh to whileLoop/whileLoop1.sh

--- whileLoop/whileLoop1.sh
+++ whileLoop/whileLoop1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+
+# Program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n till 256 is reached
 read -p "Enter a number: " n;
 power=1;
 i=0;

fb687b0e789d9853f38b460f504143e0172dd75c
Anushka25999 <48594364+Anushka25999@users.noreply.github.com>
2020-08-24 15:25:24 +0530

    Update and rename whileLoop4.sh to whileLoop/whileLoop4.sh

--- whileLoop/whileLoop4.sh
+++ whileLoop/whileLoop4.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
+
+#gambler starts with total Rs 100
 total=100;
 count=0;
 bet=0;
+
+#places Re 1 bet until he or she goes broke or reaches the goal of Rs 200
 while [[ $total -ne 0 || $total -ne 200 ]]
 do
 	i=$((RANDOM%2));

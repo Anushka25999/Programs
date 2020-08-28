a0fb4483dd5269bc4f87960689756526d20744f9
anushka <anushka25999@gmail.com>
2020-08-28 22:21:58 +0530

    Added: regexPattern.sh file

--- Patterns/regexPattern.sh
+++ Patterns/regexPattern.sh
@@ -1,15 +1,3 @@
 #!/bin/bash -x
 
-#turn on external globbing
-shopt -s extglob
-#User registration
-read -p "Enter your Last Name " lname
-
-pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
-
-if [[ $lname =~ $pat ]]
-then
-	echo "Valid Last Name"
-else
-	echo "Invalid Last Name"
-fi
+echo "Welcome to the User Registration Pattern validation"
